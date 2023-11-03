63b6d59afd3d895d70921f395f921b38f52d6896
Frederik Bode <thorbensen@gmail.com>
2023-11-03 15:41:21 +0100

    refactor: category model update

--- app/core/config.py
+++ app/core/config.py
@@ -8,13 +8,14 @@ origins = [
     "http://localhost",
     "http://localhost:3000",
     "http://localhost:8080",
+    "http://localhost:5173",
 ]
 
 details = {
     "title": "Fugazi: AI Detection Reports API 📝",
-    "description": '''The AI Detection Reports API is a microservice that provides functionality for managing and accessing reports
+    "description": """The AI Detection Reports API is a microservice that provides functionality for managing and accessing reports
     related to AI-based content detection. This service allows you to create, read, update, and delete reports 
-    on AI detection results, helping you keep track of content flagged or identified by your AI models. 🚀''',
+    on AI detection results, helping you keep track of content flagged or identified by your AI models. 🚀""",
     "version": "1.0.0",
     "contact": {
         "name": "bodeby",
--- app/models/category.py
+++ app/models/category.py
@@ -5,6 +5,6 @@ from bson import ObjectId
 
 class Category(BaseModel):
     id: str = Field(..., description="MongoDB Object ID of the document")
-    category_id: str = Field(..., description="Reference to a category document")
+    category_id: int = Field(..., description="Reference to a category document")
     name: str = Field(..., description="Category name")
     description: str = Field(..., description="Category description")
